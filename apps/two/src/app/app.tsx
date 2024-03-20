68b7445cd052dbc0f8b7d4cbc72c5301fa1fdd2d
Pavel Saikou <60567324+pavelsaikov@users.noreply.github.com>
2024-03-20 10:23:56 +0300

    commit 1

--- apps/two/src/app/app.tsx
+++ apps/two/src/app/app.tsx
@@ -2,14 +2,11 @@
 import styles from './app.module.css';
 
 import NxWelcome from './nx-welcome';
-import {TestLib} from "@pol-test/test-lib";
-
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="pol-test" />
-      <TestLib/>
+      <NxWelcome title="two" />
     </div>
   );
 }
